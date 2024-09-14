a5e4bd1e99314a39464ad072de15f626eb45e3d7
amirrezabordbar <bordbaramirreza705@gmali.com>
2024-09-15 01:43:48 +0330

    update meun in the mobile

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,13 @@
 let menu = document.querySelector(".menu");
 let menuBtn = document.querySelector(".header-icon-bar");
+let hidden = document.querySelector(".hidden");
 
 menuBtn.addEventListener("click", function () {
   menu.classList.toggle("active");
+  hidden.classList.remove("hidden")
 });
+
+hidden.addEventListener("click", function () {
+  hidden.classList.add("hidden")
+  menu.classList.remove("active");
+})
